f200860ddadc3b9a6a67f94feb27a1d7f49aaa4f
Francesco La Camera <francesco.lacamera@protonmail.com>
2020-07-03 00:54:03 +0200

    Updated script (switched from scp to rsync) and updated contact page

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-# NOTICE: This scripts removes all files from the target dir!!!
+# TODO: Replace scp with rsync
 
 USER="root"
 HOST="lacamera.xyz"
 DIR="/var/www/homepage"
 
-ssh -t $USER@$HOST "rm -rf '$DIR\*'" && scp -r [!.,!LICENSE,!README]* $USER@$HOST:$DIR
+ssh -t $USER@$HOST "rm -rf '$DIR/*'"
+rsync -ram --progress --force \
+  --exclude={'.git/','LICENSE','deploy.sh'} \
+  --include '*' \
+  . $USER@$HOST:$DIR
